99211da2a7c892f1f194275f8c2ee55ea56e0f27
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-20 20:30:57 +0545

    feat: button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <main>
         <h1>welcome to the pizza app</h1>
+        <Button variant="outline">Button</Button>
       </main>
     </>
   );
