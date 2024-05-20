43f88d9a1ade061fe868c8f92ad9e331535084bd
Th-Sagar <sagarthapa98122@gmail.com>
2024-05-20 20:49:30 +0545

    chores:theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <main>
         <h1>welcome to the pizza app</h1>
-        <Button variant="outline">Button</Button>
+        <Button>Button</Button>
       </main>
     </>
   );
